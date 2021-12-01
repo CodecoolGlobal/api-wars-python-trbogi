93198e7d7f5c69b2c0d6fd6fc6b2fe7fac0d05c5
trbogi <trencsenyi.bogi@gmail.com>
2021-12-01 14:26:43 +0100

    Set up web server

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
-# put your code here
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
