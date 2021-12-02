737893bd3717bb2f45612b564a6e977935e52a74
trbogi <trencsenyi.bogi@gmail.com>
2021-12-02 23:38:54 +0100

    Show planet information (10 planet/page)

---
+++ planets.js
@@ -0,0 +1,59 @@
+let PLANETS = "https://swapi.dev/api/planets/"
+let tableBody = document.querySelector("#tablebody")
+let nextPageLink = null
+let previousPageLink = null
+
+
+fetch(PLANETS).then(response => response.json())
+.then(data => {
+    nextPageLink = data.next
+    showHideButton(previousPageLink,previousButton)
+    showHideButton(nextPageLink,nextButton)
+    for(let i=0; i<data.results.length; i++){
+        var tr = document.createElement('TR');
+        tr.classList.add("row");
+        let dataForTable = [data.results[i]["name"], data.results[i]["diameter"], data.results[i]["climate"], data.results[i]["terrain"], data.results[i]["surface_water"], data.results[i]["population"] ]
+        for(let j=0; j<dataForTable.length; j++){
+            var td = document.createElement('TD');
+            td.classList.add("col");
+            td.innerHTML = dataForTable[j]
+            tr.appendChild(td);
+        }
+        tableBody.appendChild(tr);
+}})
+
+let nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", e => loadNextOrPreviousPage(nextPageLink))
+
+let previousButton = document.querySelector("#previous");
+previousButton.addEventListener("click", e => loadNextOrPreviousPage(previousPageLink))
+
+
+
+function showHideButton(link, buttonToLink){
+    if (link == null){
+    buttonToLink.classList.add("invisible")
+}else{
+    buttonToLink.classList.remove("invisible")
+}
+}
+
+
+function loadNextOrPreviousPage(APILink){
+    fetch(APILink).then(response => response.json())
+    .then(data => {
+        nextPageLink = data.next;
+        previousPageLink = data.previous;
+        showHideButton(previousPageLink,previousButton)
+        showHideButton(nextPageLink,nextButton)
+        let rows = tableBody.querySelectorAll(".row")
+        for(let i=0; i<data.results.length; i++){
+            var tr = rows[i]
+            let dataForTable = [data.results[i]["name"], data.results[i]["diameter"], data.results[i]["climate"], data.results[i]["terrain"], data.results[i]["surface_water"], data.results[i]["population"]]
+            let cells = tr.querySelectorAll(".col")
+                for(let j=0; j<dataForTable.length; j++){
+                    var td = cells[j]
+                    td.innerHTML = dataForTable[j]}
+        }
+    })
+}
